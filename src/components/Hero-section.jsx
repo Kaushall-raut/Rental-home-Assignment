5bb515f673da0f31ffa0e61daf09e6c62d3bc8c2
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-11-23 23:29:36 +0530

    commit for title section of hero section

---
+++ src/components/Hero-section.jsx
@@ -0,0 +1,23 @@
+import "./hero.css";
+export const Herosection = () => {
+  return (
+    <div className="container">
+      <div>
+        <div className="title">
+          <div>
+            <h1>
+              Find Your Perfect <span>Rental Home</span>
+            </h1>
+          </div>
+          <div className="text"> 
+            <p>
+              Beautifully curated rental homes that perfectly match your style
+              and needs, making your <br></br>search for the ideal living space
+              effortless and enjoyable.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
