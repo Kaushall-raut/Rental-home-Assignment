a6a203864831a1bb60768dde5cb69413126eefb0
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-11-24 00:05:24 +0530

    commit for input element of navbar

--- src/components/Hero-section.jsx
+++ src/components/Hero-section.jsx
@@ -1,3 +1,4 @@
+import { CiSearch } from "react-icons/ci";
 import "./hero.css";
 export const Herosection = () => {
   return (
@@ -9,13 +10,19 @@ export const Herosection = () => {
               Find Your Perfect <span>Rental Home</span>
             </h1>
           </div>
-          <div className="text"> 
+          <div className="text">
             <p>
               Beautifully curated rental homes that perfectly match your style
               and needs, making your <br></br>search for the ideal living space
               effortless and enjoyable.
             </p>
           </div>
+        </div>
+        <div className="navbar">
+          <input type="text" placeholder="Find your city" />
+          <span className="icon">
+          <CiSearch />
+          </span>
         </div>
       </div>
     </div>
